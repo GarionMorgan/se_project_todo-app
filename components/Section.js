bf1e954d92cbc1ec3e0538bc80c9972854d74b8d
github.com/GarionMorgan <garmorgan14@yahoo.com>
2025-09-11 13:35:03 -0500

    Added Section.js

---
+++ components/Section.js
@@ -0,0 +1,13 @@
+class Section {
+  constructor({ items, renderer, containerSelector }) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  renderItems() {
+    this._items.forEach((item) => {});
+  }
+}
+
+export default Section;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,23 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
+import Section from "../components/Section.js";
+
+// DOM Elements
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopup.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
 const todosList = document.querySelector(".todos__list");
+
+const todosSection = new Section({
+  items: initialTodos,
+  renderer: (item) => {
+    renderTodo(item);
+  },
+  containerSelector: ".todos__list",
+});
 
 const openModal = (modal) => {
   modal.classList.add("popup_visible");
